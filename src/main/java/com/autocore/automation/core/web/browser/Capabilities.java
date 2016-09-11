d93cdccb1af06453154a19b9e951dea563464ba7
Dominic Pace <dpace@nhpace.net>
2016-09-11 15:15:04 -0400

    Added more Capabilities for web browser.

--- src/main/java/com/autocore/automation/core/web/browser/Capabilities.java
+++ src/main/java/com/autocore/automation/core/web/browser/Capabilities.java
@@ -2,9 +2,14 @@ package com.autocore.automation.core.web.browser;
 
 import com.autocore.automation.core.commons.utils.exception.RuntimeInterruptionException;
 import com.autocore.automation.core.web.WebConfig;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Collections;
 
 /**
  * (C) Copyright 2016 Dominic Pace (https://github.com/Dominic-Pace)
@@ -33,23 +38,45 @@ public class Capabilities {
                 capabilities = DesiredCapabilities.firefox();
                 FirefoxProfile profile = new FirefoxProfile();
 
-                profile.setAcceptUntrustedCertificates(false);
-                profile.setAssumeUntrustedCertificateIssuer(true);
+                profile.setAcceptUntrustedCertificates(true);
+                profile.setAssumeUntrustedCertificateIssuer(false);
+                profile.setPreference(
+                        "network.automatic-ntlm-auth.trusted-uris", "http://, https//");
                 profile.setPreference("browser.helperApps.alwaysAsk.force", false);
 
                 capabilities.setCapability(FirefoxDriver.PROFILE, profile);
+                capabilities.setJavascriptEnabled(true);
 
                 break;
 
             case CHROME:
                 capabilities = DesiredCapabilities.chrome();
+                ChromeOptions options = new ChromeOptions();
+
+                options.addArguments("--start-maximized");
+
+                capabilities.setCapability("chrome.switches",
+                        Collections.singletonList("--disable-application-cache"));
+                capabilities.setJavascriptEnabled(true);
+                capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                capabilities.setCapability(ChromeOptions.CAPABILITY, options);
 
                 break;
 
             case IE:
+                capabilities = DesiredCapabilities.internetExplorer();
+
+                capabilities.setCapability(InternetExplorerDriver
+                        .INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+                capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
                 break;
 
             case SAFARI:
+                capabilities = DesiredCapabilities.safari();
+
+                capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+
                 break;
 
             default:
